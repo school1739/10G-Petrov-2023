00f6660613709977a88f0182a3158f94ab94dc81
arturchik777 <147509859+arturchik777@users.noreply.github.com>
2024-01-09 15:31:33 +0300

    Create kostandyan.py
    Классная работа 
    Костандян Артур 10 Ж

---
+++ kostandyan.py
@@ -0,0 +1,26 @@
+import random
+
+round = int(input("How many rounds you want to play"))
+p1_score = 0
+p2_score = 0
+
+def player_turn(dice)
+    result = random.randint(a:1, dice)
+    return result
+    print(result)
+
+def scoring(p1, p2)
+    global p1_score, p2_score
+    if p1>p2:
+        print("Player 2 wins")
+    elif p2>p1:
+        print("Player 2 wins")
+    else:
+        print("Friendship wins")
+
+for player_round in range(rounds):
+    player_1 = player_turn(6)
+    player_1 = player_turn(6)
+
+    scoring(player_1, player_2)
+    print(f"Player 1: {p1_score}, Player 2: {player_2}")
